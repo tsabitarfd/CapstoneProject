adb022ffb5a777700f77cd3f963b25db9060130a
Tsabita <tsabitarfd@gmail.com>
2023-05-29 13:09:28 +0700

    update

--- capstoneproject.py
+++ capstoneproject.py
@@ -2,7 +2,7 @@ import sys
 import tabulate
 import pyinputplus as pypi
 
-def showitem(dict, formatItem, title="\nSupermarket Warehouse\n"):
+def showitem(dict, title="\nSupermarket Warehouse\n"):
     #Menampilkan judul
     print(title)
     #Menampilkan dalam bentuk tabel
@@ -19,16 +19,17 @@ def additem():
         #Menambahkan barcode item
         while True:
             itemBarcode = pypi.inputInt(
-                prompt = 'Enter Barcode Item: ',)
+                prompt = 'Enter Barcode Item: ', 
+                min = 1001, 
+                max = 9999)
             #Cek barcode sudah ada atau belum pakai filter
-            filterbarcode = list(filter(lambda row: row[0] == itemBarcode, list(listItem.values())))
-            if filterbarcode != []:
+            if itemBarcode in listItem.keys():
                 print('Barcode already exist.')
                 continue
             else:
             #Menambahkan item type
                 itemType = pypi.inputStr(
-                    prompt = 'Enter Item Type: ',
+                    prompt = 'Enter Item Type. (food/non food): ',
                     applyFunc = lambda x: x.capitalize(),)
                 #Menambahkan item
                 itemName = pypi.inputStr(
@@ -40,7 +41,8 @@ def additem():
                     prompt = 'Enter Item Price: ',)
                 #Menambahkan qty
                 itemQty = pypi.inputStr(
-                    prompt = 'Enter Item Qty: ',)
+                    prompt = 'Enter Item Qty: ',
+                    blockRegexes = [r'[0-9]'])
                 #Menambhakan stock opname
                 itemStock = pypi.inputInt(
                     prompt = 'Enter Stock Item: ')
@@ -50,8 +52,8 @@ def additem():
                     if itemBarcode in list(listItem):
                         listItem[key][1] = itemType
                         listItem[key][2] = itemName
-                        listItem[key][3] = itemPrice
-                        listItem[key][4] = itemQty
+                        listItem[key][3] = itemQty
+                        listItem[key][4] = itemPrice
                         listItem[key][5] += itemStock
                     #Menambahkan item baru
                     else:
@@ -61,34 +63,112 @@ def additem():
                             [itemBarcode,
                             itemType,
                             itemName,
-                            itemPrice,
                             itemQty,
+                            itemPrice,
                             itemStock]})
+                #Menampilkan konfirmasi item sudah ditambhakan
+                print('Item added successfully.')
+                showitem(listItem)
+                addUpdate = pypi.inputYesNo(prompt = 'Do you want to add items again? (yes/no)')
+                if addUpdate == 'yes':
+                    continue
+                else:
+                    break
     else:
-
-    # #Menampilkan daftar stock gudang terbaru
+    # Menampilkan item
         showitem(listItem, formatItem)
     
-    
 
-# def updateitem():
+def updateitem():
+    #Tampilan menu update
+    while True:
+        prompt = f'Update Stock and Price\n'
+        choiceUpdate = ['Update Stock',
+                        'Update Price',
+                        'Main Menu']
+        responsechoiceUpdate = pypi.inputMenu(prompt=prompt, choices=choiceUpdate, numbered=True)
+    #Masukkan pilihan update stock item/no
+        if responsechoiceUpdate == 'Update Stock':
+            while True:
+                choiceUpdate = pypi.inputYesNo(prompt = 'Do you want to update stock items? (yes/no)')
+                #Pilih Update stock
+                if choiceUpdate == 'yes':
+                    while True:
+                        itemBarcode = pypi.inputInt(prompt = 'Enter Barcode Item: ',)
+                        #Cek barcode tidak ditemukan
+                        if itemBarcode not in listItem.keys():
+                            print('Barcode does not exist.')
+                            continue
+                        else:
+                            break
+                    updatestockItem = pypi.inputInt(prompt = 'Add new item stock: ' )
+                    #Update stock item
+                    listItem[itemBarcode][5] += updatestockItem
+                    print('Stock item update successfully')
+                    showitem(listItem)
+                else:
+                    break
+
+        elif responsechoiceUpdate == 'Update Price':
+            #Pilihan mau update harga atau tidak
+            while True:
+                choiceupdatePrice = pypi.inputYesNo(
+                    prompt = 'Do you want to update price items? (yes/no)')
+                #Pilih yes update price
+                if choiceupdatePrice == 'yes':
+                    while True:
+                        itemBarcode = pypi.inputInt(
+                            prompt = 'Enter Barcode Item: ',)
+                            #Cek barcode tidak ditemukan
+                        if itemBarcode not in listItem.keys():
+                            print('Barcode does not exist.')
+                            continue
+                        else:
+                            break
+                    updatePriceitem = pypi.inputInt(prompt = 'Add new price item: ' )
+                        #Update stock item
+                    listItem[itemBarcode][4] = updatePriceitem
+                    print('Price item update successfully')
+                    showitem(listItem)
+                else:
+                    break
+        #kembali ke menu utama
+        else:
+            break
     #Update stock opname item
+
 
 def deleteitem():
     #Masukin pilihan delete/no
-
-    #validasi barcode item yang ada
-    itemBarcode = pypi.inputInt(
-        prompt = 'Enter Barcode Item: ',
-        lessThan = 9999)
-    #Cek barcode ada atau tidak (balik menu delete item)
-
-    #Delete item dengan barcode kalau ,pilih yes
-    for key, value in listItem.copy().items():
-        if itemBarcode in value:
-            del listItem[key]
+    deleteChoice = pypi.inputYesNo(
+        prompt = 'Do you want to delete items? (yes/no)')
+    if deleteChoice == 'yes':
+        #validasi barcode item yang ada
+        while True:
+            itemBarcode = pypi.inputInt(
+                prompt = 'Enter Barcode Item: ', 
+                min = 1001, 
+                max = 9999)
+            #Cek barcode ada atau tidak (balik menu delete item)
+            if itemBarcode in listItem.keys(): 
+                #Delete item dengan barcode kalau yes dan barcode ada di list
+                for key, value in listItem.copy().items():
+                    if itemBarcode in value:
+                        del listItem[key]
+                print('Item deleted successfully.')
+                showitem(listItem)
+                deleteUpdate = pypi.inputYesNo(
+                    prompt = 'Do you want to delete items again? (yes/no)')
+                if deleteUpdate == 'yes':
+                    continue
+                else:
+                    break
+            else:    
+                itemBarcode = pypi.inputInt( 
+                prompt = 'Barcode does not exist, enter barcode again: ')
+    else:
     #Menampilkan item yang masih ada
-    showitem(listItem, formatItem)
+        main()
 
 def func(db):
     #Mengubah value list item menjadi list
@@ -128,6 +208,7 @@ def main():
                         dataitem, header = func(listItem)
                         #Memfilter data item berdasarkan barcode
                         filterbarcode = list(filter(lambda row: row[0] == responsebarcode, dataitem))
+                        #Barcode ada atau engga dalam data
                         if filterbarcode != []:
                             print(tabulate.tabulate(filterbarcode, header, tablefmt="outline"))
                         else:
@@ -156,11 +237,11 @@ if __name__ == "__main__":
     #List item
     listItem = {
         'Column' : ['Barcode', 'Item Type', 'Item', 'Qty', 'Price', 'Stock Opname'],
-        'Soap' : [3001, 'Non Food', 'Soap', 'pcs', 12000, 5],
-        'Cooking Oil' : [1002, 'Food', 'Cooking Oil', 'pcs', 14000, 7],
-        'Rice' : [1003, 'Food', 'Rice', 'pcs', 60000, 3],
-        'Lamp' : [3004, 'Non Food', 'Lamp','pcs', 35000, 8],
-        'Lotus Biscuits' : [1005, 'Food', 'Lotus Biscuits', 'pcs', 43000, 9],
+        3001 : [3001, 'Non Food', 'Soap', 'pcs', 12000, 5],
+        1001 : [1001, 'Food', 'Cooking Oil', 'pcs', 14000, 7],
+        1002 : [1002, 'Food', 'Rice', 'pcs', 60000, 3],
+        3002 : [3002, 'Non Food', 'Lamp','pcs', 35000, 8],
+        1003 : [1003, 'Food', 'Lotus Biscuits', 'pcs', 43000, 9],
         }
     #Format Item
     formatItem = "{:<2}" + "{:<12}" + "{:<13}" + "{:<18}" + "{:<8}" + "{:<10}" + "{:<15}"
